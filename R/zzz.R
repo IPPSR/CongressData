55eb09a92a042acb18ef3d0f60b494d2ddc285cd
Caleb Lucas <calebjlucas@gmail.com>
2025-08-07 13:13:11 -0400

    zzz

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,9 @@
 
 # prints when the package is attached using library()
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Please cite:\n")
-  packageStartupMessage("Grossmann, Matt, Caleb Lucas, and Benjamin Yoel. Introducing CongressData and Correlates of State Policy")
-  packageStartupMessage("East Lansing, MI: Institute for Public Policy and Social Research (IPPSR), 2024.")
+  packageStartupMessage("Please cite our paper if you use this package:\n")
+  packageStartupMessage('Grossmann, Matt, Caleb Lucas, and Benjamin Yoel. "Introducing CongressData and Correlates of State Policy." Scientific Data 12, no. 1 (2025): 1185.')
   packageStartupMessage("\nRun `CongressData::get_congress_version()` to print the version of CongressData the package is using.\n")
 }
+
+.onAttach()
